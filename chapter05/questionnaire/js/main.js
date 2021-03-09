fefb071dfab4044d40309190efe4440d5abe36c6
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2021-03-09 16:54:45 +0900

    チェックボックスの情報取得と変更

---
+++ chapter05/questionnaire/js/main.js
@@ -0,0 +1,43 @@
+$(function() {
+	// テキストをコピー
+	var copyText = function() {
+		// テキストを取得
+		var name = $("#name").val();
+
+		// テキストを設定
+		$("#name2").val(name);
+
+		// コンソールに出力
+		console.log("text", name);
+	};
+
+	// チェックボックスをコピー
+	var copyCheckbox = function() {
+		// チェックされているかを取得
+		var chck = $("#chck").prop("checked");
+
+		// チェックを設定
+		$("#chck2").prop("checked", chck);
+
+		// on/off 関係なく値を得る
+		var chckVal = $("#chck").val();
+
+		// コンソールに出力
+		console.log("checkbox", chck, chckVal);
+	};
+
+	// チェック済みチェックボックスの値を出力
+	var dumpCheckbox = function() {
+		$("input[type=checkbox]:checked").each(function() {
+			// idを得る
+			var id = $(this).attr("id");
+
+			// on/off 関係なく値を得る
+			var chckVal = $(this).val();
+
+			// コンソールに出力
+			console.log("dump checkbox", id, chckVal);
+		});
+	};
+});
+
